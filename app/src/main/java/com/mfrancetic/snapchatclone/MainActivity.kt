86ef1a445d9025d5f58f059252fea5e8ef285069
Maja Francetic <francetic.maja@gmail.com>
2020-05-11 15:18:53 +0200

    Add the new user to the Firebase database, in the folder 'users'

--- app/src/main/java/com/mfrancetic/snapchatclone/MainActivity.kt
+++ app/src/main/java/com/mfrancetic/snapchatclone/MainActivity.kt
@@ -3,14 +3,24 @@ package com.mfrancetic.snapchatclone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.UserHandle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val USERS_KEY = "users"
+
+    private val EMAIL_ID = "email";
+
+    private lateinit var database: DatabaseReference
 
     private lateinit var auth: FirebaseAuth
 
@@ -19,6 +29,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference
+
         setLoginView()
         setupOnClickListeners()
     }
@@ -45,32 +57,41 @@ class MainActivity : AppCompatActivity() {
 
     private fun signUpUser(email: String, password: String, password2: String) {
         if (password != password2) {
-            Toast.makeText(baseContext, getString(R.string.passwords_do_not_match), Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                baseContext,
+                getString(R.string.passwords_do_not_match),
+                Toast.LENGTH_SHORT
+            ).show()
             return
         }
         if (email.isBlank() || password.isBlank()) {
-            Toast.makeText(baseContext, getString(R.string.email_password_empty), Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                baseContext,
+                getString(R.string.email_password_empty),
+                Toast.LENGTH_SHORT
+            ).show()
             return
         }
         if (password.length < 3) {
-            Toast.makeText(baseContext, getString(R.string.email_password_too_short), Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                baseContext,
+                getString(R.string.email_password_too_short),
+                Toast.LENGTH_SHORT
+            ).show()
             return
         }
 
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(this.localClassName, "createUserWithEmail:success")
                     val user = auth.currentUser
+                    addUserToDatabase(task.result?.user, email)
                     Toast.makeText(
                         baseContext, getString(R.string.sign_up_success),
                         Toast.LENGTH_SHORT
                     ).show()
                     updateUi(user)
                 } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(this.localClassName, "createUserWithEmail:failure", task.exception)
                     Toast.makeText(
                         baseContext, task.exception?.message,
                         Toast.LENGTH_SHORT
@@ -80,12 +101,16 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
+    private fun addUserToDatabase(user: FirebaseUser?, email: String) {
+        if (user != null) {
+            database.child(USERS_KEY).child(user.uid).child(EMAIL_ID).setValue(email)
+        }
+    }
+
     private fun loginUser(email: String, password: String) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(this.localClassName, "signInWithEmail:success")
                     val user = auth.currentUser
                     Toast.makeText(
                         baseContext, getString(R.string.login_success),
@@ -93,8 +118,6 @@ class MainActivity : AppCompatActivity() {
                     ).show()
                     updateUi(user)
                 } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(this.localClassName, "signInWithEmail:failure", task.exception)
                     Toast.makeText(
                         baseContext, task.exception?.message,
                         Toast.LENGTH_SHORT
@@ -103,7 +126,7 @@ class MainActivity : AppCompatActivity() {
                     // ...
                 }
             }
-            }
+    }
 
     private fun setLoginView() {
         sign_up_login_button.text = getString(R.string.login)
